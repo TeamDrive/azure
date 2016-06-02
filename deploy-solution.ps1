82757e7d963517d5be2ef35db682d035e0130b16
Christian Geuer-Pollmann <christian.geuer-pollmann@web.de>
2016-06-02 12:54:24 +0200

    .

--- deploy-solution.ps1
+++ deploy-solution.ps1
@@ -5,6 +5,7 @@ $authorizedKeyFilename = "C:\Users\chgeuer\puttykeys\authorizedkeys.txt"
 $githubUser = "chgeuer"
 $githubProject = "td"
 
+
 $_ignore = & git commit -am "." -q
 $branch =  & git rev-parse HEAD
 $repositoryUrl = "https://raw.githubusercontent.com/$($githubUser)/$($githubProject)/$($branch)/"
@@ -13,15 +14,17 @@ Write-Host "Pusing to '$($repositoryUrl)'"
 $_ignore = & git push origin master -q
 
 $resourceGroupName="rg-$($tenantName)"
+$regServerInstanceCount = 2
+$portalServerInstanceCount = 0
 
 $commonSettings = @{
 	tenantName=$tenantName
 	adminUsername=$env:USERNAME.ToLower()
 	adminSecureShellKey=$(Get-Content -Path $authorizedKeyFilename).Trim()
 	longtermResourceGroupName="longterm-$($tenantName)"
-	deployRegServer="Enabled"
+	deployRegServer=$(if($regServerInstanceCount -gt 0) { "enabled" } else { "disabled" })
 	regServerInstanceCount=2
-	deployPortalServer="Enabled"
+	deployPortalServer=$(if($portalServerInstanceCount -gt 0) { "enabled" } else { "disabled" })
 	repositoryUrl=$repositoryUrl
 }
 
@@ -30,7 +33,6 @@ New-AzureRmResourceGroup `
  	-Location $location `
 	-Force
 
-#	-TemplateParameterObject @{ commonSettings=$commonSettings } `
 $deploymentResult = New-AzureRmResourceGroupDeployment `
 	-ResourceGroupName $resourceGroupName `
 	-TemplateUri "$($repositoryUrl)/ARM/azuredeploy.json" `
