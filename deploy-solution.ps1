4f6bedb43c50882263af7fafe4949fbc6d1142b5
Christian Geuer-Pollmann <christian.geuer-pollmann@web.de>
2016-06-02 11:09:05 +0200

    .

--- deploy-solution.ps1
+++ deploy-solution.ps1
@@ -1,6 +1,4 @@
-
-$tenantName = "saxony12"
-
+$tenantName = "saxony1"
 
 # $sshkey = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAklvcJCA1K82mjlxM1ZHP1PF7eqKZ5KcmxLeNSJ7vMLD1/XUMZ667P2Ep7iu/J4Ci4u6V4LyYDodhRRFZaaxMFHTUVTwcApuh0fl4UJY/Evd6R+A66zxG8oSG75KQTiOYsV4cZ1PnMfg1Y014n814VGKd68ZHKc2KQFLGfsBZ7Hqc2NIU1Y0AxNaeHG8i9FlhSSCDuk/Lyh3o+vJl3GKFMezua5+rFG+H8wPSCN9tkrf+zpW1ynyXC+xEn9eenLcryyqa9G2MYR1FgClxLTgZzAHYCnmMgsMxxvMVOlI6nLS3sFh4+14j8wgPbv4TzH3AI7PxcGbOvWGvlTsLRL/nmQ=="
 $authorizedKeyFilename = "C:\Users\chgeuer\puttykeys\authorizedkeys.txt"
@@ -16,11 +14,11 @@ $_ignore = & git push origin master -q
 
 $commonSettings = @{
 	location="West Europe"
-	tenantName=$tenantName
 	resourceGroupName="rg-$($tenantName)"
-	longtermResourceGroupName="longterm-$($tenantName)"
+	tenantName=$tenantName
 	adminUsername=$env:USERNAME.ToLower()
 	adminSecureShellKey=$(Get-Content -Path $authorizedKeyFilename).Trim()
+	longtermResourceGroupName="longterm-$($tenantName)"
 	deployRegServer="Enabled"
 	regServerInstanceCount=2
 	deployPortalServer="Enabled"
@@ -34,7 +32,7 @@ New-AzureRmResourceGroup `
 
 $deploymentResult = New-AzureRmResourceGroupDeployment `
 	-ResourceGroupName $commonSettings['resourceGroupName'] `
-	-TemplateUri "$($repositoryUrl)/ARM/shared-resources.json" `
+	-TemplateUri "$($repositoryUrl)/ARM/azuredeploy.json" `
 	-TemplateParameterObject @{ commonSettings=$commonSettings } `
 	-Mode Complete  `
 	-Force `
