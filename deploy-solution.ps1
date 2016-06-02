4c7895047ca44ba55c1c14ebacd0bb6b5cdd6e84
Christian Geuer-Pollmann <christian.geuer-pollmann@web.de>
2016-06-02 09:45:35 +0200

    .

--- deploy-solution.ps1
+++ deploy-solution.ps1
@@ -1,11 +1,19 @@
 
+# $sshkey = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAklvcJCA1K82mjlxM1ZHP1PF7eqKZ5KcmxLeNSJ7vMLD1/XUMZ667P2Ep7iu/J4Ci4u6V4LyYDodhRRFZaaxMFHTUVTwcApuh0fl4UJY/Evd6R+A66zxG8oSG75KQTiOYsV4cZ1PnMfg1Y014n814VGKd68ZHKc2KQFLGfsBZ7Hqc2NIU1Y0AxNaeHG8i9FlhSSCDuk/Lyh3o+vJl3GKFMezua5+rFG+H8wPSCN9tkrf+zpW1ynyXC+xEn9eenLcryyqa9G2MYR1FgClxLTgZzAHYCnmMgsMxxvMVOlI6nLS3sFh4+14j8wgPbv4TzH3AI7PxcGbOvWGvlTsLRL/nmQ=="
+$authorizedKeyFilename = "C:\Users\chgeuer\puttykeys\authorizedkeys.txt"
+$githubUser = "chgeuer"
+$githubProject = "td"
 
-git commit -am "."
-git push origin master
+$_ignore = & git commit -am "."
+$_ignore = & git push origin master
+
+if ($_ignore.Contains("Writing objects")) {
+	Write-Host "Pushed to github"
+}
 
 $branch = "master"
 $branch =  & git rev-parse HEAD
-$repositoryUrl = "https://raw.githubusercontent.com/chgeuer/td/$($branch)/"
+$repositoryUrl = "https://raw.githubusercontent.com/$($githubUser)/$($githubProject)/$($branch)/"
 
 Write-Host "The repo base URL is '$($repositoryUrl)'"
 
@@ -14,14 +22,13 @@ $location = "West Europe"
 $resourceGroupName = "rg-$($tenantName)"
 $longtermResourceGroupName = "longterm-$($tenantName)"
 
-$sshkey = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAklvcJCA1K82mjlxM1ZHP1PF7eqKZ5KcmxLeNSJ7vMLD1/XUMZ667P2Ep7iu/J4Ci4u6V4LyYDodhRRFZaaxMFHTUVTwcApuh0fl4UJY/Evd6R+A66zxG8oSG75KQTiOYsV4cZ1PnMfg1Y014n814VGKd68ZHKc2KQFLGfsBZ7Hqc2NIU1Y0AxNaeHG8i9FlhSSCDuk/Lyh3o+vJl3GKFMezua5+rFG+H8wPSCN9tkrf+zpW1ynyXC+xEn9eenLcryyqa9G2MYR1FgClxLTgZzAHYCnmMgsMxxvMVOlI6nLS3sFh4+14j8wgPbv4TzH3AI7PxcGbOvWGvlTsLRL/nmQ=="
 
 $parameters=@{
 	commonSettings=@{
 		tenantName=$tenantName
 		longtermResourceGroupName=$longtermResourceGroupName
 		adminUsername=$env:USERNAME.ToLower()
-		adminSecureShellKey=$sshkey
+		adminSecureShellKey=$(Get-Content -Path $authorizedKeyFilename)
 		regServerInstanceCount=2
 		deployRegServer="Enabled"
 		deployPortalServer="Enabled"
