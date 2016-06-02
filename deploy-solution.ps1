f2184cc495122c68980c5d216ecbb836fe85bfc3
Christian Geuer-Pollmann <christian.geuer-pollmann@web.de>
2016-06-02 10:21:19 +0200

    .

--- deploy-solution.ps1
+++ deploy-solution.ps1
@@ -1,5 +1,5 @@
 
-$tenantName = "saxony10"
+$tenantName = "saxony11"
 
 
 # $sshkey = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAklvcJCA1K82mjlxM1ZHP1PF7eqKZ5KcmxLeNSJ7vMLD1/XUMZ667P2Ep7iu/J4Ci4u6V4LyYDodhRRFZaaxMFHTUVTwcApuh0fl4UJY/Evd6R+A66zxG8oSG75KQTiOYsV4cZ1PnMfg1Y014n814VGKd68ZHKc2KQFLGfsBZ7Hqc2NIU1Y0AxNaeHG8i9FlhSSCDuk/Lyh3o+vJl3GKFMezua5+rFG+H8wPSCN9tkrf+zpW1ynyXC+xEn9eenLcryyqa9G2MYR1FgClxLTgZzAHYCnmMgsMxxvMVOlI6nLS3sFh4+14j8wgPbv4TzH3AI7PxcGbOvWGvlTsLRL/nmQ=="
@@ -17,17 +17,17 @@ $_ignore = & git push origin master -q
 $location = "West Europe"
 $resourceGroupName = "rg-$($tenantName)"
 $longtermResourceGroupName = "longterm-$($tenantName)"
-$parameters=@{
-	commonSettings=@{
-		tenantName=$tenantName
-		longtermResourceGroupName=$longtermResourceGroupName
-		adminUsername=$env:USERNAME.ToLower()
-		adminSecureShellKey=$(Get-Content -Path $authorizedKeyFilename).Trim()
-		regServerInstanceCount=2
-		deployRegServer="Enabled"
-		deployPortalServer="Enabled"
-		repositoryUrl=$repositoryUrl
-	}
+
+
+$commonSettings = @{
+	tenantName=$tenantName
+	longtermResourceGroupName=$longtermResourceGroupName
+	adminUsername=$env:USERNAME.ToLower()
+	adminSecureShellKey=$(Get-Content -Path $authorizedKeyFilename).Trim()
+	deployRegServer="Enabled"
+	regServerInstanceCount=2
+	deployPortalServer="Enabled"
+	repositoryUrl=$repositoryUrl
 }
 
 New-AzureRmResourceGroup `
@@ -38,7 +38,7 @@ New-AzureRmResourceGroup `
 $deploymentResult = New-AzureRmResourceGroupDeployment `
 	-ResourceGroupName $resourceGroupName `
 	-TemplateUri "$($repositoryUrl)/ARM/shared-resources.json" `
-	-TemplateParameterObject $parameters `
+	-TemplateParameterObject @{ commonSettings=$commonSettings } `
 	-Mode Complete  `
 	-Force `
 	-Verbose
