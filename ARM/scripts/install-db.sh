3607e01148b00cd3f6c43caa3ae73d16b3332a05
paul-mccullagh <paul.mccullagh@snap.de>
2016-06-14 17:10:08 +0200

    Update install-db.sh

--- ARM/scripts/install-db.sh
+++ ARM/scripts/install-db.sh
@@ -2,7 +2,7 @@
 
 # All stuff ends up in /var/lib/waagent/Microsoft.OSTCExtensions.CustomScriptForLinux-1.4.1.0/download/0
 
-yum -y update
+yum update -y --exclude=WALinuxAgent
 yum -y install mdadm
 
 # Standard fdisk partition
